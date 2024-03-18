329a38e3a80d16dc2a7d582c5396b97e4169b38b
Aashi <aashijain.60104@gmail.com>
2024-03-19 01:30:20 +0530

    signals to automatically add email expense to expense table

---
+++ apis/signals.py
@@ -0,0 +1,14 @@
+# your_app/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import MailExpense, Expense
+
+@receiver(post_save, sender=MailExpense)
+def create_expense_from_mailexpense(sender, instance, created, **kwargs):
+    if created:
+        Expense.objects.create(
+            item=instance.item,
+            amount=instance.amount,
+            category=instance.category,
+            date=instance.date_of_purchase.date()
+        )
